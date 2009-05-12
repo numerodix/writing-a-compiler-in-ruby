dd4fcc63bb9c0f05dd143269cae5c5c43332b8e2
Martin Matusiak <numerodix@gmail.com>
2009-05-12 22:44:19 +0200

    Lambdas

--- compiler.rb
+++ compiler.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
-# Writing a compiler in Ruby bottom up - step 5
-# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-5.html
+# Writing a compiler in Ruby bottom up - step 6
+# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-6.html
 
 DO_BEFORE= []
 
