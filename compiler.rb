beee17e8c4acf7bde3d113c34342b16bbb643aec
Martin Matusiak <numerodix@gmail.com>
2009-05-12 21:56:46 +0200

    Integer literals If-then-else

--- compiler.rb
+++ compiler.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
-# Writing a compiler in Ruby bottom up - step 4
-# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-4.html
+# Writing a compiler in Ruby bottom up - step 5
+# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-5.html
 
 DO_BEFORE= []
 
