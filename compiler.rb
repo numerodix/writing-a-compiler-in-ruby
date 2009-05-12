90ecaf068394cdc2ead8d0f404c2d727c543aa46
Martin Matusiak <numerodix@gmail.com>
2009-05-12 22:54:57 +0200

    Function arguments

--- compiler.rb
+++ compiler.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
-# Writing a compiler in Ruby bottom up - step 6
-# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-6.html
+# Writing a compiler in Ruby bottom up - step 7
+# http://www.hokstad.com/writing-a-compiler-in-ruby-bottom-up-step-7.html
 
 DO_BEFORE= []
 DO_AFTER= []
